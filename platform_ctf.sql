23ea519518b253813e27b9f1ea6e2e1261b73118
FajarAlnito <fajaralnito18@gmail.com>
2019-11-07 23:37:39 +0800

    nambah kategori

--- platform_ctf.sql
+++ platform_ctf.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 07, 2019 at 02:54 PM
+-- Generation Time: Nov 07, 2019 at 03:37 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.2.11
 
@@ -25,6 +25,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `id_category` int(11) NOT NULL,
+  `category` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`id_category`, `category`) VALUES
+(1, 'PWN'),
+(2, 'REVERSE'),
+(3, 'CRYPTO'),
+(4, 'FORENSIC'),
+(5, 'MISC');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `challenges`
 --
 
@@ -32,6 +54,7 @@ CREATE TABLE `challenges` (
   `id_chall` int(11) NOT NULL,
   `title` varchar(50) NOT NULL,
   `description` text NOT NULL,
+  `id_category` int(11) NOT NULL,
   `flag` varchar(50) NOT NULL,
   `poin` int(11) NOT NULL,
   `id_file` int(11) NOT NULL,
@@ -97,15 +120,29 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id_user`, `username`, `password`, `email`, `affiliation`, `role`, `status`) VALUES
+(1, 'GrizzlyMg', 'password', 'fajar@gmail.com', 'japan', 1, 1);
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`id_category`);
 
 --
 -- Indexes for table `challenges`
 --
 ALTER TABLE `challenges`
   ADD PRIMARY KEY (`id_chall`),
-  ADD KEY `id_file` (`id_file`);
+  ADD KEY `id_file` (`id_file`),
+  ADD KEY `id_category` (`id_category`);
 
 --
 -- Indexes for table `files`
@@ -138,6 +175,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `id_category` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `files`
 --
 ALTER TABLE `files`
@@ -159,7 +202,7 @@ ALTER TABLE `solves`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -169,7 +212,8 @@ ALTER TABLE `users`
 -- Constraints for table `challenges`
 --
 ALTER TABLE `challenges`
-  ADD CONSTRAINT `challenges_ibfk_1` FOREIGN KEY (`id_file`) REFERENCES `files` (`id_file`);
+  ADD CONSTRAINT `challenges_ibfk_1` FOREIGN KEY (`id_file`) REFERENCES `files` (`id_file`),
+  ADD CONSTRAINT `challenges_ibfk_2` FOREIGN KEY (`id_category`) REFERENCES `category` (`id_category`);
 
 --
 -- Constraints for table `solves`
