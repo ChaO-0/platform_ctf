a17a08af65c068e1fcb25bed212586752b7ce81c
FajarAlnito <fajaralnito18@gmail.com>
2019-11-07 22:05:10 +0800

    benerin nav

--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -63,7 +63,7 @@
             <div class="col-lg-3 col-md-6 col-sm-6 col-12">
               <div class="card card-statistic-1">
                 <div class="card-icon bg-danger">
-                  <i class="far fa-file"></i>
+                  <i class="fas fa-fire"></i>
                 </div>
                 <div class="card-wrap">
                   <div class="card-header">
--- admin/dashboard/template/nav.php
+++ admin/dashboard/template/nav.php
@@ -42,24 +42,15 @@
         <a href="index.html">Dsh.</a>
         </div>
         <ul class="sidebar-menu">
-        <li class="menu-header">Dashboard</li>
         <li class="dropdown active">
-            <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-            <ul class="dropdown-menu">
-            <li class=active><a class="nav-link" href="index-0.html">General Dashboard</a></li>
-            <li><a class="nav-link" href="index.html">Ecommerce Dashboard</a></li>
-            </ul>
+        <li>
+            <a class="nav-link" href="#"><i class="fas fa-users"></i> <span>Users</span></a></li>
         </li>
-        
-        <li class="menu-header">Starter</li>
-        <li class="dropdown">
-            <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-columns"></i> <span>Layout</span></a>
-            <ul class="dropdown-menu">
-            <li><a class="nav-link" href="layout-default.html">Default Layout</a></li>
-            <li><a class="nav-link" href="layout-transparent.html">Transparent Sidebar</a></li>
-            <li><a class="nav-link" href="layout-top-navigation.html">Top Navigation</a></li>
-            </ul>
+        <li>
+            <a class="nav-link" href="#"><i class="fas fa-fire"></i> <span>Challenge</span></a></li>
         </li>
-        <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+        <li>
+            <a class="nav-link" href="#"><i class="far fa-bell"></i> <span>Notification</span></a></li>
+        </li>
         </ul>
     </div>
