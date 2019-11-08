584c519fab12d79f4a9619169d9db4c6c4ed6a52
FajarAlnito <fajaralnito18@gmail.com>
2019-11-08 12:12:15 +0800

    nav

--- admin/dashboard/template/nav.php
+++ admin/dashboard/template/nav.php
@@ -1,9 +1,4 @@
-<?php
-    $home='./';
-    $user='users.php';
-    $chall='challenge.php';
-    $notif='notification.php';
-?>
+
 
 <nav class="navbar navbar-expand-lg main-navbar">
         <form class="form-inline mr-auto">
@@ -17,7 +12,7 @@
     
     <!-- THIS IS GREETING USER NAV -->
         <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-        <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
+        <!-- <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1"> -->
         <div class="d-sm-none d-lg-inline-block">Hi, Admin!</div></a>
         <div class="dropdown-menu dropdown-menu-right">
             <a href="features-profile.html" class="dropdown-item has-icon">
