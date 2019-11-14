1cf5668013fd0c4a12bd7b9565eb2a4a3e0c4453
FajarAlnito <fajaralnito18@gmail.com>
2019-11-14 21:44:59 +0800

    perbaiki template

--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -5,40 +5,20 @@
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
   <title>General Dashboard &mdash; Stisla</title>
 
-  <!-- General CSS Files -->
-  <link rel="stylesheet" href="assets/modules/fontawesome/css/all.min.css">
-  <link rel="stylesheet" href="assets/modules/bootstrap/css/bootstrap.min.css">
+  <?php
+    $pre=" ";
+    include 'template/css.php';
+  ?>
 
-  <!-- CSS Libraries -->
-  <link rel="stylesheet" href="assets/modules/jqvmap/dist/jqvmap.min.css">
-  <link rel="stylesheet" href="assets/modules/weather-icon/css/weather-icons.min.css">
-  <link rel="stylesheet" href="assets/modules/weather-icon/css/weather-icons-wind.min.css">
-  <link rel="stylesheet" href="assets/modules/summernote/summernote-bs4.css">
-
-  <!-- Template CSS -->
-  <link rel="stylesheet" href="assets/css/style.css">
-  <link rel="stylesheet" href="assets/css/components.css">
-
-<!-- Start GA -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-94034622-3');
-</script>
-<!-- /END GA --></head>
 
 <body>
 <div id="app">
     <div class="main-wrapper main-wrapper-1">
-      <div class="navbar-bg"></div>
       <?php
         $home='./';
-        $user='users.php';
+        $user='users/';
         $chall='challenge/';
-        $notif='notification.php';
+        $notif='notification/';
         include 'template/nav.php';
       ?>
       
@@ -203,28 +183,9 @@
     </div>
   </div>
 
-  <!-- General JS Scripts -->
-  <script src="assets/modules/jquery.min.js"></script>
-  <script src="assets/modules/popper.js"></script>
-  <script src="assets/modules/tooltip.js"></script>
-  <script src="assets/modules/bootstrap/js/bootstrap.min.js"></script>
-  <script src="assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
-  <script src="assets/modules/moment.min.js"></script>
-  <script src="assets/js/stisla.js"></script>
-  
-  <!-- JS Libraies -->
-  <script src="assets/modules/simple-weather/jquery.simpleWeather.min.js"></script>
-  <script src="assets/modules/chart.min.js"></script>
-  <script src="assets/modules/jqvmap/dist/jquery.vmap.min.js"></script>
-  <script src="assets/modules/jqvmap/dist/maps/jquery.vmap.world.js"></script>
-  <script src="assets/modules/summernote/summernote-bs4.js"></script>
-  <script src="assets/modules/chocolat/dist/js/jquery.chocolat.min.js"></script>
+  <?php
+    include 'template/js.php';
+  ?>
 
-  <!-- Page Specific JS File -->
-  <script src="assets/js/page/index-0.js"></script>
-  
-  <!-- Template JS File -->
-  <script src="assets/js/scripts.js"></script>
-  <script src="assets/js/custom.js"></script>
 </body>
 </html>
