44ab0d4bfaaaf71827ba05be4d30ae40cac6cc83
FajarAlnito <fajaralnito18@gmail.com>
2019-11-14 21:45:51 +0800

    perbaiki template

--- admin/dashboard/challenge/add.php
+++ admin/dashboard/challenge/add.php
@@ -5,34 +5,15 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
     <title>Challenge &mdash; Stisla</title>
 
-    <!-- General CSS Files -->
-    <link rel="stylesheet" href="../assets/modules/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/modules/fontawesome/css/all.min.css">
+    <?php
+    $pre="../";
+    include '../template/css.php';
+    ?>
 
-    <!-- CSS Libraries -->
-    <link rel="stylesheet" href="../assets/modules/summernote/summernote-bs4.css">
-    <link rel="stylesheet" href="../assets/modules/codemirror/lib/codemirror.css">
-    <link rel="stylesheet" href="../assets/modules/codemirror/theme/duotone-dark.css">
-    <link rel="stylesheet" href="../assets/modules/jquery-selectric/selectric.css">
-
-    <!-- Template CSS -->
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <link rel="stylesheet" href="../assets/css/components.css">
-    <!-- Start GA -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-94034622-3');
-    </script>
-    <!-- /END GA --></head>
 
     <body>
     <div id="app">
         <div class="main-wrapper main-wrapper-1">
-        <div class="navbar-bg"></div>
             <?php
             $home='../';
             $user='../users.php';
@@ -96,6 +77,12 @@
                                         </div>
                                     </div>
                                     <div class="form-group row mb-4">
+                                        <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Hint</label>
+                                        <div class="col-sm-12 col-md-7">
+                                            <input type="text" class="form-control" name="hint">
+                                        </div>
+                                    </div>
+                                    <div class="form-group row mb-4">
                                         <label class="col-form-label text-md-right col-12 col-md-3 col-lg-3">Poin</label>
                                         <div class="col-sm-12 col-md-7">
                                             <input type="number" class="form-control" name="poin">
@@ -126,25 +113,10 @@
         </div>
     </div>
 
-    <!-- General JS Scripts -->
-    <script src="../assets/modules/jquery.min.js"></script>
-    <script src="../assets/modules/popper.js"></script>
-    <script src="../assets/modules/tooltip.js"></script>
-    <script src="../assets/modules/bootstrap/js/bootstrap.min.js"></script>
-    <script src="../assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
-    <script src="../assets/modules/moment.min.js"></script>
-    <script src="../assets/js/stisla.js"></script>
-    
-    <!-- JS Libraies -->
-    <script src="../assets/modules/summernote/summernote-bs4.js"></script>
-    <script src="../assets/modules/codemirror/lib/codemirror.js"></script>
-    <script src="../assets/modules/codemirror/mode/javascript/javascript.js"></script>
-    <script src="../assets/modules/jquery-selectric/jquery.selectric.min.js"></script>
+    <?php
+        
+        include '../template/js.php';
+    ?>
 
-    <!-- Page Specific JS File -->
-    
-    <!-- Template JS File -->
-    <script src="../assets/js/scripts.js"></script>
-    <script src="../assets/js/custom.js"></script>
 </body>
 </html>
