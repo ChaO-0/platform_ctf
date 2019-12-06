659e009791d2f9da0fb29c312d60b84bbcf795a4
FajarAlnito <fajaralnito18@gmail.com>
2019-12-06 21:22:42 +0800

    apalh

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # platform_ctf 
 
-1. Download [Stisla](https://github.com/stisla/stisla) <br>
-2. Copy dan paste [assets/](https://github.com/stisla/stisla/tree/master/assets) pada folder **stisla** ke [admin/](admin/) <br>
 
-THANK YOU
+
+
--- admin/dashboard/index.php
+++ admin/dashboard/index.php
@@ -93,7 +93,7 @@
                 </div>
               </div>
             </div>
-            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+            <!-- <div class="col-lg-3 col-md-6 col-sm-6 col-12">
               <div class="card card-statistic-1">
                 <div class="card-icon bg-success">
                   <i class="fas fa-circle"></i>
@@ -107,7 +107,7 @@
                   </div>
                 </div>
               </div>
-            </div>                  
+            </div>                   -->
           </div>
 
 
