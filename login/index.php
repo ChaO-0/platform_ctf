175ef5e6d814ee029bcf6a706bf9a92872e1b517
ChaO <christophergin88@gmail.com>
2019-12-18 01:19:59 +0800

    added form validation

--- login/index.php
+++ login/index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="../assets/style.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/jquery.validate.js"></script>
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <title>Welcome</title>
@@ -59,27 +60,53 @@
         $(document).ready(function(){
             $('.sidenav').sidenav();
         });
-        $('#login').click(function (e) { 
-            var data = $('#login-form').serialize();
-            $.ajax({
-                type: "POST",
-                url: "login_process.php",
-                async: false,
-                timeout: 300000,
-                data: data,
-                success: function (response) {
-                    if(response == 1){
-                        M.toast({html: "Login success", displayLength: 1500, outDuration: 300, classes: 'rounded'});
-                        setTimeout(() => {
-                            window.location.href = "../challenges/index.php";
-                        }, 1000);
-                    }
-                    else{
-                        M.toast({html: "Wrong username or password !", displayLength: 1500, outDuration: 300, classes: 'rounded'});
-                    }
+
+        $(document).on('keypress', 'input', function(e) {
+            if(e.which == 13) {
+                $('#login').click();
+                return false;
+            }
+        });
+
+        function validate() {
+            var valid = true;
+            $("#login-form input[required], #login-form textarea[required]").each(function(){
+                $(this).removeClass('invalid');
+                $(this).attr('title','');
+                if(!$(this).val()){ 
+                    $(this).addClass('invalid');
+                    $(this).attr('data-tooltip','This field is required');
+                    $(document).ready(function(){
+                        $('.invalid').tooltip({exitDelay: 800});
+                    })
+                    valid = false;
                 }
-            });
-            
+            }); 
+            return valid;
+        }
+
+        $('#login').click(function (e) {
+            var valid = validate();
+            if(valid){
+                var data = $('#login-form').serialize();
+                $.ajax({
+                    type: "POST",
+                    url: "login_process.php",
+                    async: false,
+                    data: data,
+                    success: function (response) {
+                        if(response == 1){
+                            M.toast({html: "Login success", displayLength: 1500, outDuration: 300, classes: 'rounded'});
+                            setTimeout(() => {
+                                window.location.href = "../challenges/index.php";
+                            }, 1000);
+                        }
+                        else{
+                            M.toast({html: "Wrong username or password !", displayLength: 1500, outDuration: 300, classes: 'rounded'});
+                        }
+                    }
+                });
+            }
         });
     </script>
 </body>
