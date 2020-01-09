5c70f93b8fcf8894fa187ee6788fdde5db3eb982
FajarAlnito <fajaralnito18@gmail.com>
2020-01-09 20:47:35 +0800

    DB BOYS

--- platform_ctf.sql
+++ platform_ctf.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 07, 2020 at 08:50 AM
+-- Generation Time: Jan 09, 2020 at 12:46 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.2.11
 
@@ -56,18 +56,9 @@ CREATE TABLE `challenges` (
   `descript` text NOT NULL,
   `id_category` int(11) NOT NULL,
   `flag` varchar(50) NOT NULL,
-  `hint` varchar(50) NOT NULL,
   `poin` int(11) NOT NULL,
   `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `challenges`
---
-
-INSERT INTO `challenges` (`id_chall`, `title`, `descript`, `id_category`, `flag`, `hint`, `poin`, `status`) VALUES
-(1, 'Free Flag', '<p>Selamat Datang<br></p>', 5, 'KCTF{ini_free_flag}', '', 100, 1),
-(2, 'awn', '<p>Gak tau males, pengen PWN<br></p>', 1, 'KCTF{selamat_d4tang_pl4y3r}', '', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -81,15 +72,6 @@ CREATE TABLE `files` (
   `file_name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `files`
---
-
-INSERT INTO `files` (`id_file`, `id_chall`, `file_name`) VALUES
-(1, 2, 'photo6327898806552734212.jpg'),
-(2, 2, 'SetupProd_OffScrub.exe'),
-(4, 2, 'f.txt');
-
 -- --------------------------------------------------------
 
 --
@@ -102,13 +84,6 @@ CREATE TABLE `hint` (
   `hint` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `hint`
---
-
-INSERT INTO `hint` (`id_hint`, `id_chall`, `hint`) VALUES
-(1, 2, '<p>Hint<br></p>');
-
 -- --------------------------------------------------------
 
 --
@@ -117,8 +92,8 @@ INSERT INTO `hint` (`id_hint`, `id_chall`, `hint`) VALUES
 
 CREATE TABLE `notification` (
   `id_notif` int(11) NOT NULL,
-  `title` int(11) NOT NULL,
-  `description` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` text NOT NULL,
   `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -136,13 +111,6 @@ CREATE TABLE `solves` (
   `created_at` datetime NOT NULL,
   `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `solves`
---
-
-INSERT INTO `solves` (`id_solve`, `id_user`, `id_chall`, `user_flag`, `created_at`, `status`) VALUES
-(1, 3, 1, 'KCTF{ini_free_flag}', '2019-12-18 06:08:06', 1);
 
 -- --------------------------------------------------------
 
@@ -165,9 +133,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id_user`, `username`, `password`, `email`, `affiliation`, `role`, `status`) VALUES
-(1, 'GrizzlyMg', '5f4dcc3b5aa765d61d8327deb882cf99', 'fajar@gmail.com', 'japan', 1, 1),
-(2, 'test', '098f6bcd4621d373cade4e832627b4f6', 'test@gmail.com', 'korea', 0, 1),
-(3, 'gladys', '05fe03b494c0f1a7d6cb49f0bf3fd70d', 'gladys@gmail.com', '', 0, 1);
+(1, 'GrizzlyMg', '5f4dcc3b5aa765d61d8327deb882cf99', 'fajar@gmail.com', NULL, 1, 1);
 
 --
 -- Indexes for dumped tables
@@ -229,19 +195,19 @@ ALTER TABLE `category`
 -- AUTO_INCREMENT for table `challenges`
 --
 ALTER TABLE `challenges`
-  MODIFY `id_chall` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_chall` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `files`
 --
 ALTER TABLE `files`
-  MODIFY `id_file` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_file` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `hint`
 --
 ALTER TABLE `hint`
-  MODIFY `id_hint` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_hint` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `notification`
@@ -253,13 +219,13 @@ ALTER TABLE `notification`
 -- AUTO_INCREMENT for table `solves`
 --
 ALTER TABLE `solves`
-  MODIFY `id_solve` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_solve` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
